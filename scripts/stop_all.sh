448d83d884d699372336f4e3a2d6d3285553380d
abryazgin <abryazgin@yandex-team.ru>
2022-04-24 20:09:51 +0500

    add gitlab

--- scripts/stop_all.sh
+++ scripts/stop_all.sh
@@ -6,5 +6,7 @@ echo "Stopping microtik..."
 yc compute instance stop epd0fqnq0herulchfg77
 echo "Stopping nat..."
 yc compute instance stop epdo6g6er4kgfa40dijm
+echo "Stopping gitlab..."
+yc compute instance stop epd17scu2vmukf3bpu8g
 echo "Stopping application-load-balancer..."
 yc application-load-balancer load-balancer stop ds7u6vvmnt7d1hv8oj8u
